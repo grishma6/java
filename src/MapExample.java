9873d523434ba08bea2b1d11f72bd86353110b1a
grishma6 <gxg59150@ucmo.edu>
2025-08-25 21:32:17 -0500

    Enumeration

---
+++ src/MapExample.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class HashMap {
+    public static void main(String[] args) {
+        //HashMap = A data structure that holds key value pair
+        //          keys are unique, but can be duplicated
+        //          Doesn't maintain any order, but memory is different
+        //          HashMap<key,pair>
+
+        HashMap<String, Double> map = new HashMap<>();
+
+        map.put
+
+    }
+}
